5bc492c09433ea80760312b179946b35a96fc729
mitul72 <mituldhawan2@gmail.com>
2023-12-13 01:46:16 -0700

    Implements Kafka producer and consumer executables; Add output.txt

--- src/lib/kafka/consumer.cpp
+++ src/lib/kafka/consumer.cpp
@@ -1,16 +1,27 @@
 #include <cppkafka/cppkafka.h>
 #include <iostream>
+#include <fstream>
 #include "config_json.hpp"
+#include <vector>
+#include "calculator.hpp"
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+
 int main()
 {
+
     auto configJson = ConfigKF::GetConfigJson("config.json");
+
     cppkafka::Consumer consumer({{"metadata.broker.list", configJson["kafka"]["broker"].get<std::string>()},
                                  {"group.id", configJson["kafka"]["group"].get<std::string>()}});
 
     consumer.subscribe({configJson["kafka"]["topic"].get<std::string>()});
-
+    Calculator::Calculator mycalc;
     while (true)
     {
+        // Open the file in append mode
+        std::ofstream outputFile("output.txt", std::ios::app);
+
         cppkafka::Message msg = consumer.poll();
         if (msg)
         {
@@ -20,10 +31,69 @@ int main()
             }
             else
             {
-                std::cout << "Received message: " << msg.get_payload() << std::endl;
+                std::cout << msg.get_payload() << std::endl;
+                try
+                {
+                    double res = 0.0;
+                    // boost::multiprecision::cpp_int res_fact = 0;
+                    std::string payload = msg.get_payload();
+
+                    // Parse the JSON
+                    json messageJson = json::parse(payload);
+
+                    double op1 = messageJson["operand1"].get<double>();
+                    double op2 = messageJson["operand2"].get<double>();
+                    int _operator = messageJson["operator"].get<int>();
+                    switch (_operator)
+                    {
+                    case 0:
+                        res = (double)mycalc.Add(op1, op2);
+                        break;
+                    case 1:
+                        res = (double)mycalc.Subtract(op1, op2);
+                        break;
+                    case 2:
+                        res = (double)mycalc.Multiply(op1, op2);
+                        break;
+                    case 3:
+                        res = (double)mycalc.Divide(op1, op2);
+                        break;
+                    case 4:
+                        res = (int)mycalc.Factorial((int)op1);
+                        break;
+
+                    default:
+                        break;
+                    }
+                    std::cout << "The result is: " << res << std::endl;
+
+                    if (outputFile.is_open())
+                    {
+                        // Write data to the file
+                        outputFile << msg.get_payload() << "\n";
+                        outputFile << "The result is: " << res << "\n";
+                        // Close the file
+                        outputFile.close();
+                        std::cout << "written" << std::endl;
+                    }
+                    else
+                    {
+                        std::cerr << "Unable to open the file." << std::endl;
+                        return 1;
+                    }
+                }
+                catch (const std::exception &e)
+                {
+                    std::cerr << "Exception caught: " << e.what() << std::endl;
+                    return 1;
+                }
             }
         }
     }
+    // Close the file
+    outputFile.close();
+
+    std::cout << "Data appended to the file successfully." << std::endl;
 
     return 0;
 }
--- src/lib/kafka/producer.cpp
+++ src/lib/kafka/producer.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 #include "config_json.hpp"
+#include <vector>
 
 using namespace cppkafka;
 using json = nlohmann::json;
@@ -11,19 +12,30 @@ using json = nlohmann::json;
 int main(int argc, char *argv[])
 {
     double operand1 = 0;
-    double operand2 = 0;
-    double _operator = 0;
+    double operand2 = -1;
+    int _operator = 0;
 
     std::cout << "Enter Operand1: ";
     std::cin >> operand1;
-
-    std::cout << "Enter Operator (0 for add, 1 for subtract, 2 for multiply, 3 for divide, 4 for factorial): ";
-    std::cin >> _operator;
+    try
+    {
+        std::cout << "Enter Operator (0 for add, 1 for subtract, 2 for multiply, 3 for divide, 4 for factorial): ";
+        std::cin >> _operator;
+        if (_operator < 0 || _operator > 4)
+        {
+            throw std::out_of_range("Invalid operator. Please enter a number between 0 and 4.");
+        }
+    }
+    catch (const std::out_of_range &ex)
+    {
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return 1;
+    }
 
     if (_operator != 4)
     {
         std::cout << "Enter Operand2: ";
-        std::cin >> _operator;
+        std::cin >> operand2;
     }
     try
     {
@@ -34,20 +46,16 @@ int main(int argc, char *argv[])
         // Create the producer
         Producer producer(config);
 
-        // Produce a message for operand1
-        std::string message1 = "Operand 1: " + std::to_string(operand1);
-        producer.produce(MessageBuilder(configJson["kafka"]["topic"].get<std::string>()).partition(0).payload(message1));
+        // Create a JSON object
+        json message = {
+            {"operand1", operand1},
+            {"operator", _operator},
+            {"operand2", operand2}};
 
-        // Produce a message for the operator
-        std::string message2 = "Operator: " + std::to_string(_operator);
-        producer.produce(MessageBuilder(configJson["kafka"]["topic"].get<std::string>()).partition(0).payload(message2));
+        // Convert JSON to string
+        std::string jsonString = message.dump();
 
-        if (_operator != 4)
-        {
-            // Produce a message for operand2
-            std::string message3 = "Operand 2: " + std::to_string(operand2);
-            producer.produce(MessageBuilder(configJson["kafka"]["topic"].get<std::string>()).partition(0).payload(message3));
-        }
+        producer.produce(MessageBuilder(configJson["kafka"]["topic"].get<std::string>()).partition(0).payload(jsonString));
 
         producer.flush();
     }
